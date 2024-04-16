ce0af60a89026cb80fdaa040cb75d961a0da1b03
Mahdi Majdian <MajdianMahdi@gmail.com>
2024-04-16 20:47:18 +0330

    configure absolute paths and svgr

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,38 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import styleX from 'vite-plugin-stylex';
+import svgr from 'vite-plugin-svgr';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react(), styleX()],
+	plugins: [react(), styleX(), svgr()],
+	resolve: {
+		alias: [
+			{
+				find: '@',
+				replacement: path.resolve(__dirname, 'src'),
+			},
+			{
+				find: '@components',
+				replacement: path.resolve(__dirname, 'src/components'),
+			},
+			{
+				find: '@utilities',
+				replacement: path.resolve(__dirname, 'src/utilities'),
+			},
+			{
+				find: '@assets',
+				replacement: path.resolve(__dirname, 'src/assets'),
+			},
+			{
+				find: '@icons',
+				replacement: path.resolve(__dirname, 'src/assets/icons'),
+			},
+			{
+				find: '@pages',
+				replacement: path.resolve(__dirname, 'src/pages'),
+			},
+		],
+	},
 });
