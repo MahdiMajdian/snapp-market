ff8e28d6414af1636eb7ce7750c273d841194f80
Mahdi Majdian <MajdianMahdi@gmail.com>
2024-04-17 07:36:08 +0330

    render categories from server response

--- src/components/Categories/index.tsx
+++ src/components/Categories/index.tsx
@@ -1,11 +1,14 @@
 import stylex from '@stylexjs/stylex';
 import { CategoryItem } from '@/components';
 import { styles } from './styles';
+import { Props } from './types';
 
-function Categories() {
+function Categories({ items }: Props) {
 	return (
 		<div {...stylex.props(styles.categories)}>
-			<CategoryItem title='مواد غذایی' imageUrl={'img'} />
+			{items.map((item) => (
+				<CategoryItem key={item.id} title={item.title} imageUrl={item.image} />
+			))}
 		</div>
 	);
 }
--- src/pages/product-list/index.tsx
+++ src/pages/product-list/index.tsx
@@ -1,11 +1,30 @@
-import { Categories } from '@/components';
+import { useMemo } from 'react';
+import { useQuery } from '@tanstack/react-query';
+import { Categories } from '@components';
+import { getProductListVendor } from '@services/product-list/api';
+
+function useVendor() {
+	const { data } = useQuery({
+		queryKey: ['categories'],
+		queryFn: getProductListVendor,
+	});
+
+	return data;
+}
 
 function ProductList() {
-	return (
-		<div>
-			<Categories />
-		</div>
-	);
+	const data = useVendor();
+
+	const categories = data?.data.extra_sections.categories;
+
+	const categoriesContent = useMemo(() => {
+		if (categories) {
+			return <Categories items={categories} />;
+		}
+		return null;
+	}, [categories]);
+
+	return <div>{categoriesContent}</div>;
 }
 
 export default ProductList;
